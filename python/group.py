c1d46c68a45d39fdc7237672adbf63c702ddcc4e
Guillem Borrell <guillemborrell@gmail.com>
2022-11-27 18:27:36 +0000

    Make Python groupby a little faster

--- python/group.py
+++ python/group.py
@@ -4,4 +4,4 @@ import sys
 import pandas as pd
 
 df = pd.read_csv(sys.stdin)
-print(df.groupby("Dept", as_index=False).Weekly_Sales.mean())
+print(df.groupby("Dept", sort=False, as_index=False).Weekly_Sales.mean())
